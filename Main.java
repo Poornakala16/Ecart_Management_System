986a8bb5bcb7c91ab669b0722b83fdc04c93a361
Poornakala <poornakala1603@gmail.com>
2026-06-03 10:51:30 +0530

    Refactor Main class structure and formatting

--- Main.java
+++ Main.java
@@ -1,18 +1,11 @@
 package eCart;
-
 import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         AddProducts ap = new AddProducts();
-
         int choice;
-
         do {
-
             System.out.println("\n===== E-CART =====");
             System.out.println("1.Add Product");
             System.out.println("2.View Product");
@@ -21,24 +14,17 @@ public class Main {
             System.out.println("5.View Cart");
             System.out.println("6.Total Bill");
             System.out.println("7.Exit");
-
             System.out.print("Enter Choice: ");
             choice = sc.nextInt();
-
             switch(choice) {
-
             case 1:
-
                 System.out.print("Enter Product ID: ");
                 int pid = sc.nextInt();
                 sc.nextLine();
-
                 System.out.print("Enter Product Name: ");
                 String pname = sc.nextLine();
-
                 System.out.print("Enter Price: ");
                 double price = sc.nextDouble();
-
                 ap.addProduct(pid, pname, price);
                 break;
 
@@ -50,18 +36,14 @@ public class Main {
 
                 System.out.print("Enter Product ID: ");
                 pid = sc.nextInt();
-
                 System.out.print("Enter Quantity: ");
                 int qty = sc.nextInt();
-
                 ap.addToCart(pid, qty);
                 break;
 
             case 4:
-
                 System.out.print("Enter Product ID: ");
                 pid = sc.nextInt();
-
                 ap.removeCart(pid);
                 break;
 
